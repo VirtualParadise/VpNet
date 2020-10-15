5bec450c78ff2ea5e33c83888a2d3e7a874a37b4
Oliver Booth <me@olivr.me>
2020-10-15 16:21:21 +0100

    Use Color? parameters for IChatFunctions methods

--- VpNet/ManagedApi/System/Interfaces/ApiFunctions/IChatFunctions.cs
+++ VpNet/ManagedApi/System/Interfaces/ApiFunctions/IChatFunctions.cs
@@ -8,10 +8,10 @@
 
         void ConsoleMessage(IAvatar targetAvatar, string name, string message, TextEffectTypes effects = 0, byte red = 0, byte green = 0, byte blue = 0);
         void ConsoleMessage(int targetSession, string name, string message, TextEffectTypes effects = 0, byte red = 0, byte green = 0, byte blue = 0);
-        void ConsoleMessage(IAvatar targetAvatar, string name, string message, Color color, TextEffectTypes effects = 0);
-        void ConsoleMessage(int targetSession, string name, string message, Color color, TextEffectTypes effects = 0);
-        void ConsoleMessage(string name, string message, Color color, TextEffectTypes effects = 0);
-        void ConsoleMessage(string message, Color color, TextEffectTypes effects = 0);
+        void ConsoleMessage(IAvatar targetAvatar, string name, string message, Color? color, TextEffectTypes effects = 0);
+        void ConsoleMessage(int targetSession, string name, string message, Color? color, TextEffectTypes effects = 0);
+        void ConsoleMessage(string name, string message, Color? color, TextEffectTypes effects = 0);
+        void ConsoleMessage(string message, Color? color, TextEffectTypes effects = 0);
         void ConsoleMessage(string message);
     }
 }
