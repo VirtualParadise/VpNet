8abfa64f23a16dacea169d1969e847508e48c940
Oliver Booth <me@olivr.me>
2021-06-19 12:43:39 +0100

    Add xmldoc to VpException. Convert Reason to property

--- VpNet/ManagedApi/System/Exceptions/VpException.cs
+++ VpNet/ManagedApi/System/Exceptions/VpException.cs
@@ -4,17 +4,27 @@ using VpNet.NativeApi;
 
 namespace VpNet
 {
+    /// <summary>
+    ///     The exception that is thrown when an operation performed by the native SDK did not return
+    ///     <see cref="ReasonCode.Success" />.
+    /// </summary>
     [Serializable]
     [XmlRoot("VpException", Namespace = Global.XmlNsException)]
     public sealed class VpException : Exception
     {
-        public ReasonCode Reason;
-
-        public VpException(ReasonCode reason) : base(string.Format("VP SDK Error: {0}({1})", reason, (int)reason))
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="VpException" /> class.
+        /// </summary>
+        /// <param name="reason">The reason code.</param>
+        public VpException(ReasonCode reason) : base($"VP SDK Error: {reason} ({(int) reason})")
         {
             Reason = reason;
         }
 
-        public VpException() { }
+        /// <summary>
+        ///     Gets the reason code.
+        /// </summary>
+        /// <value>The reason code.</value>
+        public ReasonCode Reason { get; }
     }
 }
