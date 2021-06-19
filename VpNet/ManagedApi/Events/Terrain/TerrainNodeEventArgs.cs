1ebfa5c1c56f3ad74e69601ded7557c70299fc72
Oliver Booth <me@olivr.me>
2021-06-19 16:07:59 +0100

    #7 Directly implement TerrainNodeEventArgs

--- VpNet/ManagedApi/Events/Terrain/TerrainNodeEventArgs.cs
+++ VpNet/ManagedApi/Events/Terrain/TerrainNodeEventArgs.cs
@@ -7,7 +7,21 @@ namespace VpNet
     ///     Provides event arguments for <see cref="Instance.OnTerrainNode" />.
     /// </summary>
     [XmlRoot("OnTerrainNode", Namespace = Global.XmlNsEvent)]
-    public class TerrainNodeEventArgs : BaseTerrainNodeEventArgs
+    public class TerrainNodeEventArgs : TimedEventArgs
     {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="TerrainNodeEventArgs" /> class.
+        /// </summary>
+        /// <param name="terrain">The terrain.</param>
+        public TerrainNodeEventArgs(ITerrain terrain)
+        {
+            Terrain = terrain;
+        }
+
+        /// <summary>
+        ///     Gets the terrain.
+        /// </summary>
+        /// <value>The terrain.</value>
+        public ITerrain Terrain { get; }
     }
 }
