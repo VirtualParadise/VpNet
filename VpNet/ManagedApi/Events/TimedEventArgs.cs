5a51fb8732de7b4a8c4e752340e467af2814ccb8
Oliver Booth <me@olivr.me>
2021-06-21 12:09:16 +0100

    (#8) Clean up TimedEventArgs

--- VpNet/ManagedApi/Events/TimedEventArgs.cs
+++ VpNet/ManagedApi/Events/TimedEventArgs.cs
@@ -3,16 +3,15 @@ using System.ComponentModel;
 
 namespace VpNet
 {
+    /// <summary>
+    ///     Provides an additional timestamp as an event argument.
+    /// </summary>
     public abstract class TimedEventArgs : EventArgs
     {
-        private DateTime _creationDate = DateTime.UtcNow;
-
-        public DateTime CreationDateUtc
-        {
-            get { return _creationDate; }
-            set { _creationDate = value; }
-        }
-        [EditorBrowsable(EditorBrowsableState.Never)]
-        public virtual void Initialize() { }
+        /// <summary>
+        ///     Gets the date and time at which this event was fired.
+        /// </summary>
+        /// <value>The date and time at which this event was fired.</value>
+        public DateTimeOffset CreationTimestamp { get; } = DateTimeOffset.Now;
     }
 }
