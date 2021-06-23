c073374b444375cfdaa2daecc896d06cf5607022
Oliver Booth <me@olivr.me>
2021-06-23 14:58:21 +0100

    (#8) Remove Cell.Zero

--- VpNet/ManagedApi/Scene/Cell.cs
+++ VpNet/ManagedApi/Scene/Cell.cs
@@ -6,11 +6,6 @@
     public struct Cell
     {
         /// <summary>
-        ///     The center cell of the world (0, 0).
-        /// </summary>
-        public static readonly Cell Zero = new Cell(0, 0);
-        
-        /// <summary>
         ///     Initializes a new instance of the <see cref="Cell" /> struct.
         /// </summary>
         /// <param name="x">The X coordinate of the cell.</param>
